289af068aadf9aac71aaf635b2c6981fec9a7682
Ganseblum <977252487@qq.com>
2022-05-08 22:55:54 +0800

    初始化完成

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { createStore } from 'vuex'
 
+// vue2创建仓库，new Vuex.Store({})
+// vue3创建仓库，createStore({})
+
 export default createStore({
   state: {
+    username: 'zs'
   },
   getters: {
   },
